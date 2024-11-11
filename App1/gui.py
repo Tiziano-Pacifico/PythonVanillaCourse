50884f8e5d32b302612d3bf82517489b2bbb8fde
Tiziano Pacifico <gourmetmolise@gmail.com>
2024-11-11 22:14:29 +0100

    un po avanti

--- App1/gui.py
+++ App1/gui.py
@@ -3,12 +3,19 @@ import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+#add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True, size=[45,10])
-edit_button = sg.Button("Edit")
+#edit_button = sg.Button("Edit")
+
+button_labels = ["Add","Edit"]
+input_box_list = [input_box, list_box]
+layout = [[label]]
+for index, bl in enumerate(button_labels):
+    layout.append([input_box_list[index], sg.Button(bl)])
+
 
 window = sg.Window('My TODO app',
-                   layout=[[label],[input_box, add_button],[list_box, edit_button]],
+                   layout=layout,
                    font=('Helvetica',20))
 while True:
     event, values = window.read()
--- CodingExercise/11-20/16_156.py
+++ CodingExercise/11-20/16_156.py
@@ -1,14 +1,22 @@
 import FreeSimpleGUI as sg
 
-input1 = sg.Input()
+input1 = sg.Input(key="feet")
 label1 = sg.Text("Enter feet")
 
-input2 = sg.Input()
+input2 = sg.Input(key="inches")
 label2 = sg.Text("Enter inches")
 
 button_convert = sg.Button("Convert")
+output = sg.Text(key = "output")
 
-window = sg.Window("Convertor", layout=[[label1, input1], [label2, input2], [button_convert]])
-
-window.read()
+window = sg.Window("Convertor", layout=[[label1, input1], [label2, input2], [button_convert, output]])
+while True:
+    event, values = window.read()
+    match event:
+        case sg.WIN_CLOSED:
+            break
+    feet = int(values['feet'])
+    inches = int(values['inches'])
+    result = feet * inches
+    window['output'].update(value=result)
 window.close()
