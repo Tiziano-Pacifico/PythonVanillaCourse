99d4f7932b786a6fd0df97a32b74a94fd21ccc40
Tiziano Pacifico <gourmetmolise@gmail.com>
2024-11-14 21:59:44 +0100

    starting web app

--- App1/gui.py
+++ App1/gui.py
@@ -1,6 +1,23 @@
 from modules import functions
 import FreeSimpleGUI as sg
 import time
+import os
+import sys
+
+# Gestione del percorso in base all'ambiente (sviluppo o eseguibile)
+if getattr(sys, 'frozen', False):
+    base_path = sys._MEIPASS  # Percorso temporaneo nell'eseguibile
+else:
+    base_path = os.path.dirname(__file__)
+
+# Percorso delle immagini
+add_image_path = os.path.join(base_path, "images/add.png")
+complete_image_path = os.path.join(base_path, "images/complete.png")
+
+os.makedirs("Files", exist_ok=True)
+if not os.path.exists("Files/todos.txt"):
+    with open("Files/todos.txt", "w") as file:
+        pass
 
 sg.theme("LightGreen1")
 
@@ -13,8 +30,12 @@ button_labels = ["Add","Edit"]
 input_box_list = [input_box, list_box]
 layout = [[clock],[label]]
 for index, bl in enumerate(button_labels):
-    layout.append([input_box_list[index], sg.Button(bl)])
-complete_button = sg.Button("Complete")
+    if(index == 0):
+        layout.append([input_box_list[index], sg.Button(size = 2, image_source=add_image_path, mouseover_colors= "red", tooltip="Add Todo", key = "Add" )])
+    else:
+        layout.append([input_box_list[index], sg.Button(bl)])
+
+complete_button = sg.Button(size = 20, image_source=complete_image_path, mouseover_colors = "red", tooltip="Complete", key = "Complete")
 exit_button = sg.Button("Exit")
 
 layout.append([complete_button, exit_button])
@@ -45,12 +66,15 @@ while True:
             except IndexError:
                 sg.popup("Please select an item first", font=("Helvetica", 20))
         case 'Complete':
-            todo_to_complete = values['todos'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value="")
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value="")
+            except IndexError:
+                sg.popup("No todos in the list to edit")
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case 'Exit':
